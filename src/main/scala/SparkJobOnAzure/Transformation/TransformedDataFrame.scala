b152f75060a205a7f6d2d2e6916cd2ae6b252032
cutebie <66412056+cutebie@users.noreply.github.com>
2020-06-08 16:35:25 +0700

    Add files via upload

--- src/main/scala/SparkJobOnAzure/Transformation/TransformedDataFrame.scala
+++ src/main/scala/SparkJobOnAzure/Transformation/TransformedDataFrame.scala
@@ -60,9 +60,11 @@ object TransformedDataFrame {
     var retVal = true
 
     // val url = ClassLoader.getSystemResource(expectedSchemaPath)
-    // Source.fromFile causes FILE NOT FOUND if not using /dbfs
-    val expectedSchemaPath_upd = "/dbfs" + expectedSchemaPath
-    val schemaSource = Source.fromFile(expectedSchemaPath_upd).getLines.mkString
+    // Source.fromFile causes FILE NOT FOUND if not using /dbfs after dbutils.fs.mount()
+    //val expectedSchemaPath_upd = "/dbfs" + expectedSchemaPath
+    // use wasbs url instead of dbutils.fs.mount so that don't need to append /dbfs
+    //val url = ClassLoader.getSystemResource(expectedSchemaPath)
+    val schemaSource = Source.fromFile(expectedSchemaPath).getLines.mkString
     val schemaFromJson = DataType.fromJson(schemaSource).asInstanceOf[StructType]
     //println(schemaFromJson.treeString)
 
